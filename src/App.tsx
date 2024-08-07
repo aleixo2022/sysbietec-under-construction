a6b82247d0e40d90fe8404a2fdfc6deb6e8b7c1c
alexsandro <alexsandro.nv.02@gmail.com>
2024-08-07 15:25:06 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import UnderConstruction from './UnderConstruction';
+
+function App() {
+  return (
+    <div>
+      <UnderConstruction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UnderConstruction.tsx
@@ -0,0 +1,20 @@
+ 
+import { FaTools, FaHardHat, FaWrench } from 'react-icons/fa';
+import styles from './UnderConstruction.module.css';
+
+const UnderConstruction = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <FaTools className={styles.icon} />
+        <h1 className={styles.title}>Sysbietec</h1>
+        <FaHardHat className={styles.icon} />
+        <h2 className={styles.subtitle}>Em Construção</h2>
+        <FaWrench className={styles.icon} />
+        <p className={styles.message}>Nosso site está em desenvolvimento. Em breve teremos novidades!</p>
+      </div>
+    </div>
+  );
+};
+
+export default UnderConstruction;
